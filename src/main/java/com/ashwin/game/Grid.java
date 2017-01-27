e41dc3b6f9475dc1c3b15bafe19a26f64d8efb20
ashwinkonale <ashwin.konale@gmail.com>
2017-01-27 18:55:59 +0530

    Adding skeleton for grid

--- src/main/java/com/ashwin/game/Grid.java
+++ src/main/java/com/ashwin/game/Grid.java
@@ -1,5 +1,30 @@
 package com.ashwin.game;
 
+import com.ashwin.game.entity.Cell;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Grid {
     // todo construct grid and populate cell
+
+    // This will have two dimensional array of Cell object with Coordinates and surrounding cells.
+
+    List<List<Cell>> cellArray = new ArrayList<>();
+
+    public Grid(int rows, int columns) {
+
+    }
+
+    private void populateGrid(){
+        //todo
+    }
+
+    public void playgame(){
+        //todo
+    }
+
+    private void gotoNextGen(){
+        //todo
+    }
 }
